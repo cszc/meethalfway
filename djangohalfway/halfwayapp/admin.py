a62cbfa55c0eac4106c93de39aadfe6088b13e93
Leith McIndewar <lmcindewar@Leiths-MacBook-Pro.local>
2016-02-10 08:17:42 -0600

    Working on admin

--- djangohalfway/halfwayapp/admin.py
+++ djangohalfway/halfwayapp/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 from .models import Address, Participant, Meeting
 
-admin.site.register(Address, Participant, Meeting)
+admin.site.register(Address)
+admin.site.register(Participant)
+admin.site.register(Meeting)
